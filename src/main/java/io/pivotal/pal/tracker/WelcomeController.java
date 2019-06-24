df29ec4bbfffac21c380305b74bc7dfe8f9449b1
Answer <answer@AnswerdeMacBook-Pro.local>
2019-06-24 16:15:22 +0800

    a

--- src/main/java/io/pivotal/pal/tracker/WelcomeController.java
+++ src/main/java/io/pivotal/pal/tracker/WelcomeController.java
@@ -1,13 +1,22 @@
 package io.pivotal.pal.tracker;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class WelcomeController {
 
+private String cfInstanceIndex;
+
+    public WelcomeController(@Value("${welcome.message}")
+                                     String cfInstanceIndex) {
+         this.cfInstanceIndex = cfInstanceIndex;
+    }
+
     @GetMapping("/")
     public String sayHello() {
-        return "hello";
+       // return "hello";
+    return this.cfInstanceIndex;
     }
 }
