1d8e0b370d9619d4f3c2ae48257d826e1b99611e
Answer <answer@AnswerdeMacBook-Pro.local>
2019-06-25 11:37:14 +0800

    d

--- src/main/java/io/pivotal/pal/tracker/WelcomeController.java
+++ src/main/java/io/pivotal/pal/tracker/WelcomeController.java
@@ -7,16 +7,19 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class WelcomeController {
 
-private String cfInstanceIndex;
+    private String cfInstanceIndex;
 
-    public WelcomeController(@Value("${welcome.message}")
-                                     String cfInstanceIndex) {
-         this.cfInstanceIndex = cfInstanceIndex;
+    // welcome.nessage在不同的启动方式下输入值的方式也不一样
+    // 1、直接main函数启动，在run configuration的environment variable设置WELCOME_MESSAGE=asdfg
+    // 2、使用bootRun方式启动，在build.gradle中的bootRun.environment设置
+    // 3、cf push在manifest.yml文件中设置env
+    public WelcomeController(@Value("${welcome.message}") String cfInstanceIndex) {
+        this.cfInstanceIndex = cfInstanceIndex;
     }
 
     @GetMapping("/")
     public String sayHello() {
-       // return "hello";
-    return this.cfInstanceIndex;
+//         return "hello";
+        return this.cfInstanceIndex;
     }
 }
